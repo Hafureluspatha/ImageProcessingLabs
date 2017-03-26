a601e1cd34912cc687a2db9ea94ef27d2e93467e
Hafureluspatha <xxxddr3@gmail.com>
2017-03-26 10:33:34 +0300

    Complete control work

--- badretdinov_d_f/prj/4ControlWork1/controlWork1.cpp
+++ badretdinov_d_f/prj/4ControlWork1/controlWork1.cpp
@@ -5,51 +5,6 @@
 using namespace cv;
 using namespace std;
 
-void changeImage(int leftTrackbarPosition, int rightTrackbarPosition)
-{
-	/*vector<Mat> channels;
-	vector<vector<int>> hist(3, vector<int>(256));
-	correctedImage = originalImage.clone();
-	split(correctedImage, channels);
-	for (int i = 0; i < correctedImage.rows; i++)
-	{
-		for (int j = 0; j < correctedImage.cols; j++)
-		{
-			hist[0][(int)(channels[0].at<uchar>(i, j))]++;
-			hist[1][(int)(channels[1].at<uchar>(i, j))]++;
-			hist[2][(int)(channels[2].at<uchar>(i, j))]++;
-		}
-	}
-
-	int leftMin(255);
-	int rightMax(0);
-	vector<int> bounds;
-
-	for (int i = 0; i < hist.size(); ++i)
-	{
-		bounds = countBoundsOfOneChannel(leftTrackbarPosition, rightTrackbarPosition, hist[i]);
-		if (bounds[0] < leftMin) {
-			leftMin = bounds[0];
-		}
-		if (bounds[1] > rightMax) {
-			rightMax = bounds[1];
-		}
-	}
-	for (int i = 0; i < correctedImage.rows; i++)
-	{
-		for (int j = 0; j < correctedImage.cols; j++)
-		{
-			channels[0].at<uchar>(i, j) = saturate_cast<uchar>(linearTransformationFunction(leftMin, rightMax,
-				static_cast<int>(channels[0].at<uchar>(i, j))));
-			channels[1].at<uchar>(i, j) = saturate_cast<uchar>(linearTransformationFunction(leftMin, rightMax,
-				static_cast<int>(channels[1].at<uchar>(i, j))));
-			channels[2].at<uchar>(i, j) = saturate_cast<uchar>(linearTransformationFunction(leftMin, rightMax,
-				static_cast<int>(channels[2].at<uchar>(i, j))));
-		}
-	}
-	merge(channels, correctedImage);*/
-}
-
 int main(int argc, char* argv[])
 {
 	Mat image(400, 1200, CV_8UC1);
@@ -86,10 +41,22 @@ int main(int argc, char* argv[])
 	circle(image, Point(1100, 300), 80, white, -1, 8, 0);
 
 	Mat outputImage;
-	int sz[2][2] = { {1, 0}, {0, -1} };
-	Mat kernel(2, sz, CV_8UC(1), Scalar::all(0));
-	//
-	filter2D(image, outputImage, CV_8U, kernel, Point(-1, -1), 0, BORDER_DEFAULT);
-	imshow("", image);
+	char ker[2][2] = { { 1, 0 }, { 0, -1 } };
+	
+	Mat kernel(2, 2, CV_8SC(1), ker);
+
+	filter2D(image, outputImage, CV_32F, kernel, Point(-1, -1), 0, BORDER_DEFAULT);
+	outputImage *= 127 / 255.0;
+	for (int i = 0; i < outputImage.rows; ++i)
+	{
+		for (int j = 0; j < outputImage.cols; ++j)
+		{
+			outputImage.at<float>(i, j) += 127;
+		}
+	}
+	Mat convertedOutputImage;
+	outputImage.convertTo(convertedOutputImage, CV_8U);
+
+	imshow("", convertedOutputImage);
 	waitKey(0);
 }
